986e3b05c8b4e3649a7ecbeda6465bf291cadd96
Benjamin Parks <Benparks101@gamil.comgit config --global user.email Benparks101@gamil.com>
2022-01-19 15:04:47 -0600

    replace space with char or string edabit question

---
+++ WordCharWord/src/CharBetweenWords.java
@@ -0,0 +1,6 @@
+public class CharBetweenWords {
+
+    public static String add(String c, String str) {
+        return str.replace(" ", c);
+    }
+}
---
+++ WordCharWord/src/Main.java
@@ -0,0 +1,9 @@
+//Create a function that will put the first argument, a character, between every word in the second argument, a string.
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(CharBetweenWords.add("&", " I love Tam"));
+    }
+}
