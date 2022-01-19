a859fe67a0a304066ca558850c61041a040e896e
Benjamin Parks <Benparks101@gamil.comgit config --global user.email Benparks101@gamil.com>
2022-01-19 14:44:34 -0600

    Sum all arrays edabit problem

---
+++ Get_Sum_of_All_Array_Elements/src/Main.java
@@ -0,0 +1,10 @@
+// Create a function that takes an array and returns the sum of all numbers in the array.
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 7, 4};
+
+        System.out.println(Program.arraySum(arr));
+    }
+}
