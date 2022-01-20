5007caa14b6fdecf2e3671c5c450b9a88734b0d4
Benjamin Parks <Benparks101@gamil.comgit config --global user.email Benparks101@gamil.com>
2022-01-20 13:26:43 -0600

    on\of Switch edabit problem

---
+++ On_Off_Switch/src/Main.java
@@ -0,0 +1,10 @@
+//Create a function that returns how many possible arrangements can come from a certain number of
+// switches (on / off). In other words, for a given number of switches, how many different
+// patterns of on and off can we have?
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Program.posCom(3));
+    }
+}
---
+++ On_Off_Switch/src/Program.java
@@ -0,0 +1,6 @@
+public class Program {
+
+    public static int posCom(int n) {
+        return (int) Math.pow(2,n);
+    }
+}
