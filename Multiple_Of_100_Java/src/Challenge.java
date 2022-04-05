67fe74d7d2221dcb5396c27ac744780bd45d9e1d
Benjamin Parks <Benparks101@gamil.comgit config --global user.email Benparks101@gamil.com>
2022-04-05 15:36:44 -0500

    Edabit Java

---
+++ Multiple_Of_100_Java/src/Challenge.java
@@ -0,0 +1,10 @@
+public class Challenge {
+
+    public static boolean divisible(int num) {
+        if (num % 100 == 0) {
+            return true;
+        } else  {
+            return false;
+        }
+    }
+}
