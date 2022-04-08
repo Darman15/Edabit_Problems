3000569f44517678208c302e2bc9b3b3ed643c66
Benjamin Parks <Benparks101@gamil.comgit config --global user.email Benparks101@gamil.com>
2022-04-08 13:19:12 -0500

    Edabit JavaScript

---
+++ BooleanToString_JavaScript/main.js
@@ -0,0 +1,11 @@
+const boolToString = (flag) => {
+  return String(flag);
+};
+
+const boolToStringTwo = (flag) => {
+  return `${flag}`;
+};
+
+console.log(true);
+
+console.log(typeof boolToStringTwo(true));
