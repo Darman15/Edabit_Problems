95322b47820f927fbc9c247051eda0c4a2c43ccd
Benjamin Parks <Benparks101@gamil.comgit config --global user.email Benparks101@gamil.com>
2022-04-19 15:23:46 -0500

    problem to find star number in java

---
+++ Nth_Star_Number_Java/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(starNumber(3));
+    }
+
+    public static int starNumber(int n) {
+        return ((6 * n) * (n -1)) + 1;
+    }
+}
