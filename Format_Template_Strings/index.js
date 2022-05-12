f2d9ca7600deccd0e6ad16c90a8d69886e3ccfd5
Benjamin Parks <Benparks101@gamil.comgit config --global user.email Benparks101@gamil.com>
2022-05-12 10:20:19 -0500

    Problem with template strings

---
+++ Format_Template_Strings/index.js
@@ -0,0 +1,14 @@
+
+
+const a = "John"
+const b = "Joe"
+const c = "Jack"
+
+
+const format = (a, b, c) => {
+    const template = `Their names were: ${a}, ${b}, and ${c}.,`
+    console.log(template)
+}
+
+format(a, b, c);
+
